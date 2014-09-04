9dff56b99cae568eee2a774599a9704eed3000d3
David Harris <david.harris@bramhalldata.com>
2014-09-04 08:42:34 -0400

    show node details

--- cookbooks/my_cookbook/metadata.rb
+++ cookbooks/my_cookbook/metadata.rb
@@ -5,3 +5,6 @@ license          'Apache 2.0'
 description      'Installs/Configures my_cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'build-essential'
+depends 'apache2', '>= 1.0.4'
---
+++ vendor/bundle/ruby/1.9.1/gems/octokit-3.3.0/lib/octokit/version.rb
@@ -0,0 +1,7 @@
+module Octokit
+
+  # Current version
+  # @return [String]
+  VERSION = "3.3.0".freeze
+
+end
